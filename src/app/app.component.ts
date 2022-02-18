14ed6fae9b784fea9cd16489ddd0ead819777727
alvaro.rodrigues <alvaro.rodrigues@mvrec.local>
2022-02-18 10:34:38 -0300

    templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+//executamos a função, a component, passando os metadados
+//como o selector atraves de um objeto javascrip e na sequencia
+//nos definimos a classe a baixo que recebe esses metadados
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+//classe
+//O objetivo de uma classe e gerenciar o template
+//controlar toda a logica
 export class AppComponent {
   title = 'app1';
 }
