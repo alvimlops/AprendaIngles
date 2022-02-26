95472a24ad3742f3e8dc2936573899b27c90efb5
alvaro.rodrigues <alvaro.rodrigues@mvrec.local>
2022-02-26 12:24:21 -0300

    Add @input

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -13,12 +13,15 @@ export class PainelComponent implements OnInit {
   public frases: Frase[] = FRASES
   public instrucao: string = 'Traduza a frase'
   public resposta: string = ''
+
   public rodada: number = 0
+  public rodadaFraseEng!: Frase;
 
+  public progresso:number = 0
 
-  public progresso: number = 0
   public tentativas: number = 3
   rodadaFrase: any;
+
 
   constructor() {
     this.atualizaRodada()
@@ -49,7 +52,11 @@ export class PainelComponent implements OnInit {
 
 
     }else{
-      alert('a tradução esta errada')
+      this.tentativas--
+
+      if(this.tentativas === -1){
+        alert('voce perdeu todas as tentativas')
+      }
     }
 
   }
--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Coracao } from '../shared/coracao.model';
 
 @Component({
@@ -10,6 +10,10 @@ export class TentativasComponent implements OnInit {
 
   public coracaoVazio:string = '/assets/coracao_vazio.png'
   public coracaoCheio:string = '/assets/coracao_cheio.png'
+
+
+ @Input()
+  public tentativas!: number;
 
   public coracoes: Coracao[] = [
     new Coracao(true), new Coracao(true), new Coracao(true)
